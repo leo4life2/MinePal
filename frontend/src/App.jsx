ea133289cae48e1949b407c0697ab1e3eecf1cfa
Leo Li <leo4life@163.com>
2024-07-07 22:18:18 -0700

    local host

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,9 @@ import { useState, useEffect } from 'react'
 import axios from 'axios'
 import './App.css'
 
-const api = axios.create();
+const api = axios.create({
+  baseURL: LOCAL_BE_HOST
+});
 
 function App() {
   const [settings, setSettings] = useState({
