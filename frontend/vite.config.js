05b842e60eaa54a543d204d09546961daa44402c
Leo Li <leo4life@163.com>
2024-07-07 19:12:18 -0700

    update be host

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
   },
   define: {
-    PROD_BE_HOST: JSON.stringify('https://backend.minepal.net'),
+    PROD_BE_HOST: JSON.stringify('https://backend.minepal.net:19999'),
     TEST_BE_HOST: JSON.stringify('http://10.0.0.235:19999'),
     LOCAL_BE_HOST: JSON.stringify('http://localhost:19999')
   },
