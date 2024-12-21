2e6422ffca9108f87fe08ca8b21a351f5958fab9
Leo Li <leo4life@163.com>
2024-12-20 21:06:39 -0800

    dont minify react

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,11 +2,35 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react({
+    swcOptions: {
+      jsc: {
+        transform: {
+          react: {
+            development: true,
+            throwIfNamespace: true,
+          }
+        }
+      }
+    }
+  })],
   server: {
   },
   define: {
     LOCAL_BE_HOST: JSON.stringify('http://localhost:10101')
   },
   base: './', // Ensure assets are loaded relative to the index.html location
+  build: {
+    minify: false,
+    rollupOptions: {
+      output: {
+        manualChunks: undefined,
+      },
+    },
+    sourcemap: true,
+    target: 'esnext',
+  },
+  esbuild: {
+    keepNames: true,
+  }
 });
