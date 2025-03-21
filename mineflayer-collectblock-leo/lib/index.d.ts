6ca2f83d0dbfdc25f22cb3164711014c5b935741
Leo Li <leo4life@163.com>
2025-03-20 17:07:30 -0700

    wipe old modules

--- mineflayer-collectblock-leo/lib/index.d.ts
+++
@@ -1,9 +0,0 @@
-import { Bot } from "mineflayer";
-import { CollectBlock, Callback, CollectOptions } from "./CollectBlock";
-declare module "mineflayer" {
-    interface Bot {
-        collectBlock: CollectBlock;
-    }
-}
-export declare function plugin(bot: Bot): void;
-export { CollectBlock, Callback, CollectOptions };
